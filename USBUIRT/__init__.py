c5448c11459b681bb8e8ca8bc948d31ab8bf6aaa
jeff <jeff@sonnabendfamily.org>
2023-03-07 16:24:44 -0600

    transmit and receive working

--- USBUIRT/__init__.py
+++ USBUIRT/__init__.py
@@ -1,31 +1,3 @@
-# -*- coding: utf-8 -*-
-#
-# This file is a plugin for EventGhost.
-# Copyright © 2005-2020 EventGhost Project <http://www.eventghost.net/>
-#
-# EventGhost is free software: you can redistribute it and/or modify it under
-# the terms of the GNU General Public License as published by the Free
-# Software Foundation, either version 2 of the License, or (at your option)
-# any later version.
-#
-# EventGhost is distributed in the hope that it will be useful, but WITHOUT
-# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-# FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
-# more details.
-#
-# You should have received a copy of the GNU General Public License along
-# with EventGhost. If not, see <http://www.gnu.org/licenses/>.
-
-"""<rst>
-Hardware plugin for the `USB-UIRT <http://www.usbuirt.com/>`_ transceiver.
-
-|
-
-.. image:: picture.jpg
-   :align: center
-   :target: http://www.usbuirt.com/
-"""
-
 from ctypes import (
     c_int, c_uint, c_ulong, byref, c_ubyte, c_char_p, c_void_p, POINTER,
     WINFUNCTYPE, Structure, GetLastError, create_string_buffer, WinDLL,
